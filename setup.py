dd0270cbf72417ac822ced4c500f17db1f694baa
박선영 <106055642+sunny3535@users.noreply.github.com>
2025-02-03 18:34:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="im2025",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # 필요한 패키지가 있다면 여기에 추가 (예: ['numpy', 'pandas'])
+)
