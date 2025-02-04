284fdbd065d7354d09b2534003cb0204c0c2344c
박선영 <106055642+sunny3535@users.noreply.github.com>
2025-02-04 09:38:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="im2025",
     version="0.1",
     packages=find_packages(),
-    install_requires=[],  # 필요한 패키지가 있다면 여기에 추가 (예: ['numpy', 'pandas'])
+    install_requires=['ffmpeg-python',],  # 필요한 패키지가 있다면 여기에 추가 (예: ['numpy', 'pandas'])
 )
